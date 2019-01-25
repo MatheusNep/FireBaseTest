e1265e5799406299841b08df0dd62645d653f3e6
MatheusNep <matheussaboia@hotmail.com>
2019-01-25 14:54:32 -0200

    Commit, tipo: comitando primeiro post

--- app/src/main/java/com/cursoandroid/firebaseapp/firebaseapp/MainActivity.java
+++ app/src/main/java/com/cursoandroid/firebaseapp/firebaseapp/MainActivity.java
@@ -1,16 +1,21 @@
 package com.cursoandroid.firebaseapp.firebaseapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference databaseReferencia = FirebaseDatabase.getInstance().getReference();
     private DatabaseReference usuarioReferencia = databaseReferencia.child("usuarios");
-    private DatabaseReference produtosReferencia = databaseReferencia.child("produtos");
+    private DatabaseReference produtosReferencia = databaseReferencia.child("produtos").child("001");
 
 
     @Override
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
         Usuario usuario = new Usuario();
         usuario.setNome("Ana Helena");
         usuario.setSobrenome("Silva");
@@ -31,7 +37,19 @@ public class MainActivity extends AppCompatActivity {
         produtos.setCor("Preto");
         produtos.setDescricao("Celular com potência máxima");
 
-        produtosReferencia.child("001").setValue(produtos);
+        produtosReferencia.child("001").setValue(produtos);*/
+
+        usuarioReferencia.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.i("FAREBASE", dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
